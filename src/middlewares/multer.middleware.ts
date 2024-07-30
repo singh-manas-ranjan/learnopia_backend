f2f7de6b635d8099fdbaa06f1a480e913a83c620
Manas Ranjan Singh <manassingh900@gmail.com>
2024-07-30 16:53:21 +0530

    creating file if not present for temp images

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -1,8 +1,16 @@
 import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const tempDir = path.join(__dirname, "../../public/temp");
+
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/public/temp");
+    cb(null, tempDir);
   },
   filename: function (req, file, cb) {
     const id = req.params.id;
